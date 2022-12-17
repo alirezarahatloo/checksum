4e4a919daea8482b0c0d931923308ef81a796180
Rahatloo Alireza <rahatlooalireza@gmail.com>
2022-12-17 15:11:11 +0330

    message

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ uint8_t checksum(uint8_t *cal,uint8_t size)
 }
 int main()
 {
-    uint8_t cal_checksum[array_size] = {0x02,0x21,0x00}; 
+    uint8_t cal_checksum[array_size] = {0x02,0x23,0x00}; 
    // uint8_t checkSum = cal_checksum[0];
     // for(uint8_t i = 1 ; i < array_size ; i++)
     // {

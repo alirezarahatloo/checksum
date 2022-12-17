029dbb3755b6909c300982f1fba1f9d9608cbf44
Rahatloo Alireza <rahatlooalireza@gmail.com>
2022-12-17 18:49:57 +0330

    message

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ uint8_t checksum(uint8_t *cal,uint8_t size)
 int main()
 {
     uint8_t cal_checksum[array_size] = {0x02,0x23,0x00}; 
+    //// loop 
    // uint8_t checkSum = cal_checksum[0];
     // for(uint8_t i = 1 ; i < array_size ; i++)
     // {
     //     checkSum ^= cal_checksum[i];  
     // }
+    ///recursive
     printf("hello world:0x%x\n",checksum(cal_checksum,0));
     //printf("checksum is : %d",checkSum);
     return 0;
